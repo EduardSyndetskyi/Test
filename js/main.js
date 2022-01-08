1a96ab3f0758e1763af873ca93d94c70b0cace5f
Eduard Syndetskyi <eduard.syndetskyi@gmail.com>
2022-01-08 23:19:29 +0200

    Project for student

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const number = 24;
+
+if ( number === 0 && number <= 10 ) {
+    console.log('Доброе утро');
+} else if ( number > 10 && number <= 18 ) {
+    console.log('Добрый дынь');
+} else {
+    console.log('Добрый вечер');
+}   
